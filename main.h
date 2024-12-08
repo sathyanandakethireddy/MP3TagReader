a6725497fdb21db7aad4d74b9a0fcb33748cb91f
K.M.SATHYANANDA REDDY <sathyananda.kethireddy@gmail.com>
2024-12-08 10:19:09 +0530

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+typedef enum
+{
+    FAILURE,
+    SUCCESS,
+}STATUS;
+
+void little_to_end(int *size);
+void display_help() ;
